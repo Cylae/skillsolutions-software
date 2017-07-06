b46d64a93c5d8b1d2365fe17c25ad76dc6526a11
Cylae <alexandre.calais@me.com>
2017-07-07 01:32:11 +0200

    team & form added

--- js/custom.js
+++ js/custom.js
@@ -15,3 +15,8 @@ $(function() {
         $(".navbar-collapse").collapse('hide');
     });
 })
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
